5b82773ebbab2797f7e5702e23d4024a03a99866
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-02-19 22:26:18 +0530

    Added : Arithmetic Computation - 1

--- arithmeticComputationAndSorting.sh
+++ arithmeticComputationAndSorting.sh
@@ -6,3 +6,8 @@ echo "Arithmetic Compution And Sorting"
 read -p "Enter first input :" firstInput
 read -p "Enter second input:" secondInput
 read -p "Enter third input :" thirdInput
+
+#COMPUTE ARITHMETIC OPERATION
+result=`awk 'BEGIN{printf("%0.2f",'$firstInput' + '$secondInput' * '$thirdInput')}'`
+echo "Result:" $result
+
