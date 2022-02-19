cc568ed311d47a42c661333884b6dea2e05679a0
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-02-19 22:19:29 +0530

    Added : To take three inputs

---
+++ arithmeticComputationAndSorting.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+echo "Arithmetic Compution And Sorting"
+
+#TO TAKE INPUT FROM USER
+read -p "Enter first input :" firstInput
+read -p "Enter second input:" secondInput
+read -p "Enter third input :" thirdInput
